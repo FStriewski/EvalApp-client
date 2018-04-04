e74707189f186c36d37950b6c543c1bdefe3441b
FStriewski <fstriewski@outlook.com>
2018-04-04 09:54:37 +0200

    Status

--- src/components/EvalForm.js
+++ src/components/EvalForm.js
@@ -7,11 +7,14 @@ export default class EvalForm extends PureComponent {
     render() {
         return (
             <form id="EvalForm">
+            <p>Some date indication</p>
+
                 <div className="evalButtons" style={{ display: "flex", flexDirection: 'column' }}>
                     <button id="greenButton" type="submit">Green</button>
                     <button id="yellowButton" type="submit">Yellow</button>
                     <button id="redButton" type="submit">Red</button>
                 </div>
+                <br/>
                 <div>
                     <label htmlFor="password">Remarks</label>
                     <input type="text" name="remarks" id="remarks" />
