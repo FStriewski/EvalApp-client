04b5288457eb6f8f17ec6f26a031611ea789d725
FStriewski <fstriewski@outlook.com>
2018-04-04 10:09:29 +0200

    Add container routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import TitleBar from './containers/TitleBar';
 import LogInContainer from './containers/LogInContainer';
 import BatchContainer from './containers/BatchContainer';
@@ -9,14 +10,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TitleBar />
-        {/* <LogInContainer/> */}
-        {/* <BatchContainer />  */}
-        {/* <StudentListContainer /> */}
-        <EvalContainer />
-        This is the app
-      </div>
+      <Router>
+        <div className="App">
+          <TitleBar />
+
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+
+          <Route exact path="/login" component={LogInContainer} />
+          <Route exact path="/batches" component={BatchContainer} />
+          <Route exact path="/students" component={StudentListContainer} />
+          <Route exact path="/evaluation" component={EvalContainer} />
+
+        </div>
+      </Router>
     );
   }
 }
