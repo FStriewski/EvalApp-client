ac708e4b19ee4646da8f5785d7269ba1b4599780
FStriewski <fstriewski@outlook.com>
2018-04-05 19:46:52 +0200

    Add titlebar route

--- src/containers/TitleBar.js
+++ src/containers/TitleBar.js
@@ -6,7 +6,7 @@ export default class TitleBar extends PureComponent {
     render() {
         return (
                 <nav className="navbar navbar-light bg-danger">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand" href="/batches">
                     <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt=""/>
                             Student Evaluations
                      </a>
