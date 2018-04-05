fd06b78c86db339fc32d33797c095c4c7e087edb
FStriewski <fstriewski@outlook.com>
2018-04-06 01:04:34 +0200

    Status

--- src/containers/StudentListContainer.js
+++ src/containers/StudentListContainer.js
@@ -41,12 +41,33 @@ import StudentTile from '../components/StudentTile'
         // Array of students with an evaluation, latest first
          let withEval = students
                         .filter(x => x.evaluations.length > 0)
-                        .sort().reverse()
+                       // .map(x => x.evaluations.date.sort().reverse() )
 
-
+        // let sorter = (arr) => {
+        //     let sortedArray = arr.sort( (a,b) => b.date - a.date   ) //.sort().reverse()
+        //     console.log(JSON.stringify(sortedArray))
+        //     return sortedArray
+        // }
+        
         let dontEval = withEval
-            .filter(x => x.evaluations[0].date === adate )
-         
+            .filter(x => x.evaluations[0].date === today ) 
+                // console.log("this" + sorted.date  )
+                // return sorted.date === today 
+        
+        let noEval = students.filter(x => x.evaluations.length === 0 )
+
+         let redGroup = withEval
+             .filter(x => x.evaluations[x.evaluations.length - 1].grade === "red"
+                 && x.evaluations[x.evaluations.length - 1].date !== today)
+
+         let yellowGroup = withEval
+             .filter(x => x.evaluations[x.evaluations.length - 1].grade === "red" && x.evaluations[0].date !== today)
+
+         let greenGroup = withEval
+             .filter(x => x.evaluations[x.evaluations.length - 1].grade === "red" && x.evaluations[0].date !== today)
+
+             let test = redGroup.map(x => x.name)
+         let test2 = noEval.map(x => x.name)
     //    let array =  ["2018-03-01", "2018-01-01" ]
     //   console.log( array.sort().reverse )
 
@@ -55,7 +76,9 @@ import StudentTile from '../components/StudentTile'
       
          //console.log(y)
          console.log("withEval" + JSON.stringify(withEval) )
-         console.log("dontEval" + JSON.stringify( dontEval[0].name) )
+         console.log("dontEval" + JSON.stringify( dontEval.name) )
+         console.log("reds" + JSON.stringify(test))
+         console.log("noEval" + JSON.stringify(test2))
      }
 
   
