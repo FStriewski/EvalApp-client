09f5506be37c869005c1f0196429f465fe6deab5
FStriewski <fstriewski@outlook.com>
2018-04-06 08:28:30 +0200

    Status

--- src/containers/logic.js
+++ src/containers/logic.js
@@ -4,9 +4,49 @@ export const getGroups = (students) => {
     const today = new Date().toJSON().slice(0, 10)
     const adate = "2018-04-05"
 
+    let  sorter = (x, y) => {
+        if (x.date < y.date)
+            return -1;
+        if (x.date > y.date)
+            return 1;
+        return 0;
+    }
+
+   let students2 = [
+
+        {id:1, evaluations:[
+            {id:1, date: "2018-04-05"},
+            { id: 11, date: "2017-04-05" },
+            ],
+       },
+
+        {
+           id: 2, evaluations: [
+               { id: 2, date: "2018-04-05" },
+               { id: 22, date: "2017-04-05" },
+            ]
+        }
+    ]
+
     // Array of students with an evaluation, latest first
-    let withEval = students
-        .filter(x => x.evaluations.length > 0)
+    let withEval = students2
+        .filter(x => x.evaluations.length > 0) 
+        .filter( x =>  console.log(   x.evaluations
+            //.filter(y => console.log(y.date === "2018-04-05"  )   )
+            .filter(y => y.date === "2018-04-05").length
+            > 0
+        ) 
+    )
+
+
+
+       // .sort(x => x.evaluations.sort(sorter).reverse() )
+    
+    // {
+    //     students.map(
+    //         (student, index) => {
+
+    //             const lastEval = student.evaluations.sort()
 
 
     // let sorter = (arr) => {
@@ -27,10 +67,10 @@ export const getGroups = (students) => {
             && x.evaluations[x.evaluations.length - 1].date !== today)
 
     let yellowGroup = withEval
-        .filter(x => x.evaluations[x.evaluations.length - 1].grade === "red" && x.evaluations[0].date !== today)
+        .filter(x => x.evaluations[x.evaluations.length - 1].grade === "yelow" && x.evaluations[0].date !== today)
 
     let greenGroup = withEval
-        .filter(x => x.evaluations[x.evaluations.length - 1].grade === "red" && x.evaluations[0].date !== today)
+        .filter(x => x.evaluations[x.evaluations.length - 1].grade === "green" && x.evaluations[0].date !== today)
 
     let test = greenGroup.map(x => x.id)
     let test2 = noEval.map(x => x.name)
@@ -42,9 +82,9 @@ export const getGroups = (students) => {
 
     //console.log(y)
     console.log("withEval" + JSON.stringify(withEval))
-    console.log("dontEval" + JSON.stringify(dontEval.name))
-    console.log("greens" + JSON.stringify(test))
-    console.log("noEval" + JSON.stringify(test2))
+    // console.log("dontEval" + JSON.stringify(dontEval.name))
+    // console.log("greens" + JSON.stringify(test))
+    // console.log("noEval" + JSON.stringify(test2))
 
     return {
         red: redGroup.length,
