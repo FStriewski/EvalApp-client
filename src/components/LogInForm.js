4f8334cd6ec6b9d9bb29c5fe03c9cce22d7a5238
FStriewski <fstriewski@outlook.com>
2018-05-15 21:50:47 +0200

    Fix input form

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -20,21 +20,21 @@ export default class LogInForm extends PureComponent {
 
     render() {
         return (
-                <form id="LogInForm" onSubmit={this.handleSubmit}>
+            <form id="LogInForm" onSubmit={this.handleSubmit} autoComplete="off">
+                <p>Use for testing: tt@tt.tt  tttttt </p>
                         
                 <div className="form-group">
                             <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" name="email" id="email" value={this.state.email} onChange={this.handleChange}aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <input type="email" className="form-control" autoComplete="nope" name="email" id="email" value={this.state.email} onChange={this.handleChange}aria-describedby="emailHelp" placeholder="Enter email"/>
                         </div>
 
                 <div className="form-group">
                             <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" name="password" id="password" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
+                    <input type="password" className="form-control" autoComplete="nope" name="password" id="password" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
                         </div>
 
                 <button type="submit" className="btn btn-success">Login</button>
-
-                    </form>
+            </form>
         )
     }
 }
